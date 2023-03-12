321820bac0240a5408c54df9b69eb8b5cdac50ab
Roista57 <91594792+Roista57@users.noreply.github.com>
2023-03-12 17:22:53 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -149,7 +149,8 @@ def get_hparams(init=True):
                       help='Model name')
   
   args = parser.parse_args()
-  model_dir = os.path.join("./logs", args.model)
+  #model_dir = os.path.join("./logs", args.model)
+  model_dir = os.path.join("./content/drive/My Drive/Colab Notebooks/logs", args.model)
 
   if not os.path.exists(model_dir):
     os.makedirs(model_dir)
