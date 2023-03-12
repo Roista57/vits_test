b22773d94cfcd373160a7ad3dfc5165c75e7ae97
Roista57 <91594792+Roista57@users.noreply.github.com>
2023-03-12 17:06:49 +0900

    Update train.py

--- train.py
+++ train.py
@@ -44,10 +44,11 @@ def main():
 
   n_gpus = torch.cuda.device_count()
   os.environ['MASTER_ADDR'] = 'localhost'
-  os.environ['MASTER_PORT'] = '80000'
+  os.environ['MASTER_PORT'] = '8000'
 
   hps = utils.get_hparams()
-  mp.spawn(run, nprocs=n_gpus, args=(n_gpus, hps,))
+  #mp.spawn(run, nprocs=n_gpus, args=(n_gpus, hps,))
+  run(0, n_gpus, hps)
 
 
 def run(rank, n_gpus, hps):
